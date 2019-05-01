b4e210215edf27696313583feda2670ad9731836
biox <binary0x@gmail.com>
2019-05-01 03:34:47 +0200

    Add comments to the extensions method class

--- Game_UI/Tools/ExtentionMethods.cs
+++ Game_UI/Tools/ExtentionMethods.cs
@@ -6,13 +6,18 @@ using System.Windows.Threading;
 
 namespace Game_UI.Tools
 {
+    /// <summary>
+    /// Extension methods
+    /// </summary>
     public static class ExtentionMethods
     {
-        //private static Action EmptyDelegate = delegate () { };
+        /// <summary>
+        /// Refresh a given portion af the user interface then pause during a time laps
+        /// </summary>
+        /// <param name="uiElement">UI element needed to refresh</param>
+        /// <param name="laps">pause time laps in "ms"</param>
         public static void Refresh(this UIElement uiElement, int laps)
         {
-            //uiElement.Dispatcher.Invoke(DispatcherPriority.Render, EmptyDelegate);
-            //Thread.Sleep(50);
             var actualTask = new Task(() => uiElement.UpdateLayout());
             actualTask.Wait(laps);
         }
